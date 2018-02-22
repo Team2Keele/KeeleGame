ffc7cbb78e2808e80725df9a2e31b34717b7fab1
JemsCo <james.copping97@gmail.com>
2018-02-22 13:07:06 +0000

    Added base bufferstrat

--- Keele PacMan/src/com/team2/pacman/window/Game.java
+++ Keele PacMan/src/com/team2/pacman/window/Game.java
@@ -1,21 +1,25 @@
 package com.team2.pacman.window;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
     public static final String VERSION = "0.1a";    //Keele PacMan game version
-    
+
     private boolean running = false;    //State of the game thread
     private Thread thread;              //Game thread
-    
-    public synchronized void start(){
+
+    public synchronized void start() {
         //Stops making multiple threads of one that is already running
-        if(running)
+        if (running) {
             return;
-        
+        }
         //if the game is not already running then start a new thread
         running = true;
+
         thread = new Thread(this);
         thread.start();
     }
@@ -25,47 +29,57 @@ public class Game extends Canvas implements Runnable {
         long lastTime = System.nanoTime();
         double amountOfUpdates = 60.0;
         double ns = 1000000000 / amountOfUpdates;
-        double delta = 0 ;
+        double delta = 0;
         long timer = System.currentTimeMillis();
         int updates = 0;
         int frames = 0;
-        
-        while(running){
+
+        while (running) {
             long now = System.nanoTime();
-            delta += (now - lastTime)/ns;
+            delta += (now - lastTime) / ns;
             lastTime = now;
-            
-            while(delta >= 1){
+            while (delta >= 1) {
                 update();
                 updates++;
                 delta--;
             }
             render();
             frames++;
-            
-            if(System.currentTimeMillis() - timer > 1000){
+
+            if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
                 int fps = frames;
                 int ticks = updates;
-                System.out.println("FPS: " + fps + "TICKS: " + ticks);
+                System.out.println("FPS: " + fps + " TICKS: " + ticks);
                 updates = 0;
                 frames = 0;
             }
-            
+
         }
-        
+
     }
 
-    
-    private void update(){
+    private void update() {
     }
-    
-    private void render(){
+
+    private void render() {
         
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        
+        //Draw Everything here
+        g.setColor(Color.black);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        
+        
+        g.dispose();
+        bs.show();
     }
-    
-    
-    
+
     public static void main(String args[]) {
         Window window = new Window(800, 600, "Keele PacMan ver: " + VERSION, new Game());
     }
--- Keele PacMan/src/com/team2/pacman/window/Window.java
+++ Keele PacMan/src/com/team2/pacman/window/Window.java
@@ -17,6 +17,7 @@ public class Window {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        
+        game.start();
     }
-
 }
