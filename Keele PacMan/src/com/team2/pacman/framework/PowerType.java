30d8932dd40b6f9bcd15fadf9958e9c79f2da7ee
elliottloxley <elliottloxley@gmail.com>
2018-02-23 01:28:32 +0000

    Created method in PowerType to randomly select Power
    -also added player template for testing

---
+++ Keele PacMan/src/com/team2/pacman/framework/PowerType.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team2.pacman.framework;
+
+import java.util.Random;
+
+/**
+ *
+ * @author elliott
+ */
+public enum PowerType 
+{
+    SPEED, SLOW, ENEMY_SLOW, MULTIPLIER, ENEMY_VULNERABLE;
+    
+    public static PowerType getRandomPower()
+    {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
