6a4c6bdfb6a08e9b0841d2bdfefb07a155082297
James Copping <james.copping97@gmail.com>
2018-02-28 17:24:33 +0000

    Changed Sprite to allow, map bgimg and more animation
    added some functions and constructors to help with game implementation. 
    added a few test imgs that function as a TEST map and a TEST sprite
    which work just fine

--- Keele PacMan/src/com/team2/pacman/window/Game.java
+++ Keele PacMan/src/com/team2/pacman/window/Game.java
@@ -1,5 +1,6 @@
 package com.team2.pacman.window;
 
+import com.team2.pacman.framework.*;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -9,12 +10,18 @@ public class Game extends Canvas implements Runnable {
 
     public static final String VERSION = "0.1a";    //Keele PacMan game version
     public GameState state = GameState.START;
-    
+
     private boolean running = false;    //State of the game thread
     private Thread thread;              //Game thread
+
     
-    
-    
+    /*Testing map and sprites rendering abilities*/
+    private Map map1;
+    private Sprite testSprite1 = new Sprite("\\\\ufs.stf.keele.ac.uk\\homes\\My Documents\\NetBeansProjects\\KeeleGame-master\\Keele PacMan\\src\\com\\team2\\pacman\\res\\spriteTest.png", 16, 2);
+
+    public Game() {
+        this.map1 = new Map(16f);
+    }
 
     public synchronized void start() {
         //Stops making multiple threads of one that is already running
@@ -62,52 +69,63 @@ public class Game extends Canvas implements Runnable {
     }
 
     private void update() {
-        if(null != state) //TODO handle the changes of game states and the updates of every tick of each entity and collision
-        
-        switch (state) {
-            case START: //handle the start of the game / menu stuff
-                break;
-        
-            case RUNNING: //normal game loop
-                break;
-            case END: //handle the finishing of the game / win or lose.
-                break;
-            default:
-                break;
+        if (null != state) //TODO handle the changes of game states and the updates of every tick of each entity and collision
+        {
+            switch (state) {
+                case START: //handle the start of the game / menu stuff
+                 
+                    break;
+
+                case RUNNING: //normal game loop
+                    break;
+                case END: //handle the finishing of the game / win or lose.
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
     private void render() {
-        
+
         BufferStrategy bs = this.getBufferStrategy();
-        if(bs == null){
+        if (bs == null) {
             this.createBufferStrategy(3);
             return;
         }
         Graphics g = bs.getDrawGraphics();
-        
+
         //Draw Everything here
         g.setColor(Color.black);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        
+
+        //draw map
+        map1.render(g);
+        testSprite1.render(g, testSprite1.getCurrentFrame(), 200, 200);
         
         g.dispose();
         bs.show();
     }
-    
-    public void reset(){
+
+    public void reset() {
         //TODO handle all value resets here: player lives, score, enemy, map, timer, powerups... everything.
-        
+
         //set the game state to start again
         changeState(GameState.START);
     }
-    
-    public void changeState(GameState newState){
+
+    public void changeState(GameState newState) {
         state = newState;
     }
 
     public static void main(String args[]) {
         Window window = new Window(800, 600, "Keele PacMan ver: " + VERSION, new Game());
+    }
+
+    public enum GameState {
+        START,
+        RUNNING,
+        END
     }
 
 }
