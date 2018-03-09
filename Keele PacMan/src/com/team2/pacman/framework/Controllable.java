e555b863902d91d8711ecd456da97046a8d46ef1
elliottloxley <elliottloxley@gmail.com>
2018-03-09 01:27:28 +0000

    Merged many functions from player and enemy into controllable
    -changed enemy and player so they worked with these changes.

--- Keele PacMan/src/com/team2/pacman/framework/Controllable.java
+++ Keele PacMan/src/com/team2/pacman/framework/Controllable.java
@@ -1,12 +1,17 @@
 package com.team2.pacman.framework;
 
 import java.awt.Point;
+import java.awt.geom.Rectangle2D;
 
 public abstract class Controllable extends Entity
 {
     
     protected Tile currentTile;
     protected Tile nextTile;
+    protected Direction currentDirection;
+    protected Direction nextMove;
+    protected int xOffset;
+    protected int yOffset;
     
     public static enum Direction
     {
@@ -21,13 +26,36 @@ public abstract class Controllable extends Entity
         }
     }
     
-    protected Direction currentDirection;
-    
-    public Controllable(Map map, Tile startTile, Point.Float pos, Point size)
+    public Controllable(Map map, Tile startTile, float relativeSize) throws InvalidStartTileException
     {
-        super(map, pos, size);
+        super(map, getOffsetPos(relativeSize, startTile, map), getOffsetSize(relativeSize, startTile, map));
         currentTile = startTile;
         currentDirection = Direction.NONE;
+        nextMove = Direction.NONE;
+        if(currentTile.isWall())
+        {
+            throw(new Controllable.InvalidStartTileException("Start Tile cannot be a wall"));
+        }
+    }
+    
+    private static Point.Float getOffsetPos(float percentOffset, Tile tile, Map map)
+    {
+        Rectangle2D.Float tileRect = map.getBoundingBox(tile);
+        
+        float xPos = tileRect.x + (tileRect.width * ((1 - percentOffset) / 2)); 
+        float yPos = tileRect.y + (tileRect.height * ((1 - percentOffset) / 2));
+                        
+        return new Point.Float(xPos, yPos);
+    }
+    
+    private static Point getOffsetSize(float percentOffset, Tile tile, Map map)
+    {
+        Rectangle2D.Float tileRect = map.getBoundingBox(tile);
+        
+        int width = (int)(tileRect.width * percentOffset); 
+        int height = (int)(tileRect.height * percentOffset);
+        
+        return new Point(width, height);
     }
     
     public abstract void collide(Tile tile);
@@ -36,5 +64,59 @@ public abstract class Controllable extends Entity
     public boolean isAtJunction() 
     {
         return tileMap.isTileJunction(currentTile);
+    }
+    
+        public void center()
+    {
+        position.x = tileMap.getBoundingBox(currentTile).x + xOffset;
+        position.y = tileMap.getBoundingBox(currentTile).y + yOffset;
+    }
+
+    public void turn(Direction dir)
+    {
+        
+        nextMove = dir;
+        switch(dir)
+        {
+            case UP:
+                calculateTurn(dir, Direction.DOWN, 0, -velocityMag);
+                break;
+                
+            case DOWN:
+                calculateTurn(dir, Direction.UP, 0, velocityMag);
+                break;
+                
+            case LEFT:
+                calculateTurn(dir, Direction.RIGHT, -velocityMag, 0);
+                break;
+                
+            case RIGHT:
+                calculateTurn(dir, Direction.LEFT, velocityMag, 0);
+                break;
+        }
+    }
+    
+    private void calculateTurn(Direction moveDir, Direction checkDir, float velocityX, float velocityY)
+    {
+        if((isContainedBy(currentTile) && isAtJunction()) ||
+            currentDirection == checkDir  || currentDirection == Direction.NONE)
+        {
+            velocity.setLocation(new Point.Float(velocityX, velocityY));
+            nextTile = tileMap.getTileAdjacent(currentTile, moveDir);
+            currentDirection = moveDir;
+            nextMove = Direction.NONE;
+            if(isContainedBy(currentTile))
+            {
+                center();
+            }
+        }
+    }
+    
+    public void stop()
+    {
+        velocity.setLocation(new Point.Float(0, 0));
+        nextTile = null;
+        currentDirection = Direction.NONE;
+        center();
     }
 }
--- Keele PacMan/src/com/team2/pacman/framework/Player.java
+++ Keele PacMan/src/com/team2/pacman/framework/Player.java
@@ -16,44 +16,16 @@ public class Player extends Controllable
 {
     
     private Powerup currentPower;
-    private Direction nextMove;
     private int xOffset;
     private int yOffset;
 
     
     public Player(Map map, Tile currentTile, float relativeSize) throws InvalidStartTileException
     {
-        super(map, currentTile, getOffsetPos(relativeSize, currentTile, map), getOffsetSize(relativeSize, currentTile, map));
+        super(map, currentTile, relativeSize);
         
         this.xOffset = (int)(map.getBoundingBox(currentTile).width * ((1 - relativeSize) / 2));
         this.yOffset = (int)(map.getBoundingBox(currentTile).height * ((1 - relativeSize) / 2));
-        
-        nextMove = Direction.NONE;
-        
-        if(currentTile.isWall())
-        {
-            throw(new Controllable.InvalidStartTileException("Start Tile cannot be a wall"));
-        }
-    }
-    
-    private static Point.Float getOffsetPos(float percentOffset, Tile tile, Map map)
-    {
-        Rectangle2D.Float tileRect = map.getBoundingBox(tile);
-        
-        float xPos = tileRect.x + (tileRect.width * ((1 - percentOffset) / 2)); 
-        float yPos = tileRect.y + (tileRect.height * ((1 - percentOffset) / 2));
-                        
-        return new Point.Float(xPos, yPos);
-    }
-    
-    private static Point getOffsetSize(float percentOffset, Tile tile, Map map)
-    {
-        Rectangle2D.Float tileRect = map.getBoundingBox(tile);
-        
-        int width = (int)(tileRect.width * percentOffset); 
-        int height = (int)(tileRect.height * percentOffset);
-        
-        return new Point(width, height);
     }
         
     @Override
@@ -122,60 +94,6 @@ public class Player extends Controllable
     {
         
     }    
-    
-    public void center()
-    {
-        position.x = tileMap.getBoundingBox(currentTile).x + xOffset;
-        position.y = tileMap.getBoundingBox(currentTile).y + yOffset;
-    }
-
-    public void turn(Direction dir)
-    {
-        
-        nextMove = dir;
-        switch(dir)
-        {
-            case UP:
-                calculateTurn(dir, Direction.DOWN, 0, -velocityMag);
-                break;
-                
-            case DOWN:
-                calculateTurn(dir, Direction.UP, 0, velocityMag);
-                break;
-                
-            case LEFT:
-                calculateTurn(dir, Direction.RIGHT, -velocityMag, 0);
-                break;
-                
-            case RIGHT:
-                calculateTurn(dir, Direction.LEFT, velocityMag, 0);
-                break;
-        }
-    }
-    
-    private void calculateTurn(Direction moveDir, Direction checkDir, float velocityX, float velocityY)
-    {
-        if((isContainedBy(currentTile) && isAtJunction()) ||
-            currentDirection == checkDir  || currentDirection == Direction.NONE)
-        {
-            velocity.setLocation(new Point.Float(velocityX, velocityY));
-            nextTile = tileMap.getTileAdjacent(currentTile, moveDir);
-            currentDirection = moveDir;
-            nextMove = Direction.NONE;
-            if(isContainedBy(currentTile))
-            {
-                center();
-            }
-        }
-    }
-    
-    public void stop()
-    {
-        velocity.setLocation(new Point.Float(0, 0));
-        nextTile = null;
-        currentDirection = Direction.NONE;
-        center();
-    }
     
     public void buff(Powerup power)
     {
