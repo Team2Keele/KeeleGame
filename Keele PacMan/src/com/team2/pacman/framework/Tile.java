32ea3af631e7dd6ca2512a9c90023667031ccc51
JemsCo <james.copping97@gmail.com>
2018-03-20 00:30:25 +0000

    Added Tile sprites and all color animation for player

--- Keele PacMan/src/com/team2/pacman/framework/Tile.java
+++ Keele PacMan/src/com/team2/pacman/framework/Tile.java
@@ -14,13 +14,28 @@ public class Tile {
     private TileType type;
     private Point gridIndex;
     private Entity collectable;
+    private Sprite sprite;
+    private Point tileSize;
 
-    public Tile(Point index, TileType type, Entity collectable) {
-        gridIndex = index;
+    public Tile(Point index, TileType type, Point tileSize, Entity collectable) {
+        this.gridIndex = index;
+        this.tileSize = tileSize;
         this.type = type;
         this.collectable = collectable;
-    }
 
+        switch (this.type) {
+            case WALL:
+                this.sprite = new Sprite("wall.png", 16, 1, 0);
+                break;
+            case EDGE:
+                this.sprite = new Sprite("edge.png", 16, 1, 0);
+                break;
+            case NONE:
+                this.sprite = new Sprite("floor.png", 16, 1, 0);
+                break;
+        }
+    }
+    
     public Entity getCollectable() {
         return collectable;
     }
@@ -41,23 +56,8 @@ public class Tile {
         return gridIndex;
     }
 
-    public void testRender(Graphics g, Map m) {
-        Rectangle2D.Float tRect = m.getBoundingBox(this);
+    public void render(Graphics g) {
 
-        switch (type) {
-            case WALL:
-                g.setColor(Color.red);
-                break;
-            case EDGE:
-                g.setColor(Color.MAGENTA);
-                break;
-            case NONE:
-                g.setColor(Color.GREEN);
-                break;
-        }
-
-        g.fillRect((int) tRect.x, (int) tRect.y, (int) tRect.width, (int) tRect.height);
-        g.setColor(Color.BLACK);
-        g.drawRect((int) tRect.x, (int) tRect.y, (int) tRect.width, (int) tRect.height);
+        sprite.render(g, gridIndex.x * tileSize.x, gridIndex.y * tileSize.y, tileSize.x, tileSize.y);
     }
 }
